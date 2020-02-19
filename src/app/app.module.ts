618467e69c6e88a98b55e92198db530b9edc907c
PositiveEric <eriks.gravitis@gmail.com>
2020-02-19 19:07:53 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ValidationFormComponent } from "./validation-form/validation-form.component";
 
 @NgModule({
   declarations: [
+    ValidationFormComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/validation-form/validation-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+
+@Component({
+  selector: "app-validation-form",
+  templateUrl: "./validation-form.component.html",
+  styleUrls: ["./validation-form.component.scss"]
+})
+export class ValidationFormComponent implements OnInit {
+  validationForm: FormGroup;
+  output = "";
+  isNotNumber: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.validationForm = this.fb.group({ input: "", output: "" });
+  }
+
+  ngOnInit(): void {}
+
+  onKeyUp(event: any) {
+    this.isNotNumber = false;
+    const input = event.target.value;
+    if (isNaN(input)) {
+      this.isNotNumber = true;
+      this.output = "";
+    } else {
+      if (input > 0) {
+        if (input % 3 === 0 && input % 5 === 0) {
+          this.output = "FooBar";
+        } else if (input % 5 === 0) {
+          this.output = "Bar";
+        } else if (input % 3 === 0) {
+          this.output = "Foo";
+        } else this.output = "";
+      }
+    }
+  }
+}
