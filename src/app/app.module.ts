b09ef2ec42b715ec484610606caf39852f5d953c
PositiveEric <eriks.gravitis@gmail.com>
2020-02-20 11:54:45 +0200

    Prettier and ESlint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { ReactiveFormsModule } from "@angular/forms";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { ValidationFormComponent } from "./validation-form/validation-form.component";
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ValidationFormComponent } from './validation-form/validation-form.component';
 
 @NgModule({
   declarations: [
